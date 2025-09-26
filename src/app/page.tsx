33607a043d6eb4909ddd6403f76f6d93c3991696
Carlos Canet <carlos@carlosca.net>
2025-09-27 01:46:00 +0200

    feat: add main page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,142 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col min-h-dvh items-center justify-center p-4">
+      <header className="flex flex-col backdrop-blur-[2px] px-8 pt-8 pb-2 rounded-2xl mb-8">
+        <h1 className="mb-4 text-4xl font-semibold text-center font-[Griffy] text-primary">Advienté</h1>
+        <p className="mb-6 text-xl text-center font-[Griffy] text-base-content">
+          Bienvenides a la página del advienté 2025. <br /> Proximamen<span className="italic">té</span> más información...
+        </p>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex flex-col gap-5 px-4 md:px-8">
+        <section className="collapse collapse-arrow bg-base-100 border border-accent">
+          <input type="radio" name="my-accordion-2" defaultChecked />
+          <div className="collapse-title font-semibold">
+            ¿Qué es Advienté? <span className="badge badge-xs badge-warning font-sans">Nuevo</span>
+          </div>
+          <div className="collapse-content text-sm">
+            {" "}
+            Advienté es una iniciativa de unes cuantes amantes del <i>té</i> que hacen un calendario de adviento de <i>té</i>. Si <i>te</i> interesa, contacta con
+            nosotres.
+          </div>
+        </section>
+        <section className="collapse collapse-arrow bg-base-100 border border-accent">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title font-semilight">
+            {" "}
+            Advien<i>té</i> 2025: <span className="font-black italic">Caoslendario</span>
+          </div>
+          <div className="collapse-content text-sm">
+            <p>Después de la maravillosa experiencia del año pasado este año será así:</p>
+            <ul className="list-none">
+              <li className="before:content-['🍵_']">
+                Podrá haber tanta gente como quiera, se empieza el 1 de diciembre y se acaba cuando se acabe (en 2026, 2027 o lo que sea)
+              </li>
+              <li className="before:content-['🍵_']">
+                Si decides participar se te asignará un número, y deberás buscar un té o infusión que te guste, comprarlo, dividirlo en tantas
+                personas como seamos (con un mínimo de 24) e idear una forma de marcar su número en todas las porciones (sobrecitos y escrito a mano o
+                pegatinas o lo que sea)
+              </li>
+              <li className="before:content-['🍵_']">
+                Ir a Correos y comprar 2 sobres-bolsa prefranqueados. En uno de ellos escribir su dirección y dejarlo vacío. En el otro meter el
+                primer sobre vacío y todas las bolsitas de té. Posteriormente, envíarselo a la persona responsable de esta edición
+              </li>
+              <li className="before:content-['🍵_']">
+                La persona responsable de esta edición, hace la magia de la redistribución de bolsitas, y envía a cada participante su sobre
+                (previamente vacío) con tantas bolsas como participantes, con mucho té, infusión y amor
+              </li>
+              <li className="before:content-['🍵_']">
+                Podéis pensar en qué música, imágenes o historias os gustaría crear para ambientar vuestro día y contribuir al{" "}
+                <span className="font-semibold">
+                  ❝adivina<i>té</i>❞
+                </span>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="collapse collapse-arrow bg-base-100 border border-accent">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title font-semilight">
+            <span className="font-extrabold italic ">Caoslendario: </span>Fechas límite{" "}
+            <span className="badge badge-xs badge-error font-sans">¡IMPORTANTE!</span>
+          </div>
+          <div className="collapse-content text-sm">
+            <ul>
+              <li className="before:content-['🫖']"><span className="font-bold">30/09/2025:</span> Cierre de participantes</li>
+              <li className="before:content-['🫖']">
+                <span className="font-bold">31/10/2025:</span> Fecha tope para haber recibido el sobre con las bolsitas individuales y sobre prefranqueado
+              </li>
+              <li className="before:content-['🫖']">
+                <span className="font-bold">15/11/2025:</span> Fecha tope para enviar los detalles inspiradores sobre vuestro té o infusión de vuestro día
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        {/* <div className="card bg-base-100 w-96 shadow-sm">
+          <div className="card-body">
+            <h2 className="card-title justify-center font-[Griffy] font-semibold">
+              ¿Qué es Advienté? <span className="badge badge-xs badge-warning font-sans">Nuevo</span>
+            </h2>
+            <p>
+              Advienté es una iniciativa de unes cuantes amantes del <i>té</i> que hacen un calendario de adviento de té. Si te interesa, contacta con
+              nosotres.
+            </p>
+          </div>
         </div>
+
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <div className="card-body">
+            <h2 className="card-title justify-center font-[Griffy] font-light">
+              Advienté 2025: <span className="font-black italic">Caoslendario</span>
+            </h2>
+            <p>Después de la maravillosa experiencia del año pasado este año será así:</p>
+            <ul className="list-none">
+              <li className="before:content-['🍵']">
+                Podrá haber tanta gente como quiera, se empieza el 1 de diciembre y se acaba cuando se acabe (en 2026, 2027 o lo que sea)
+              </li>
+              <li className="before:content-['🍵']">
+                Si decides participar se te asignará un número, y deberás buscar un té o infusión que te guste, comprarlo, dividirlo en tantas
+                personas como seamos (con un mínimo de 24) e idear una forma de marcar su número en todas las porciones (sobrecitos y escrito a mano o
+                pegatinas o lo que sea)
+              </li>
+              <li className="before:content-['🍵']">
+                Ir a Correos y comprar 2 sobres-bolsa prefranqueados. En uno de ellos escribir su dirección y dejarlo vacío. En el otro meter el
+                primer sobre vacío y todas las bolsitas de té. Posteriormente, envíarselo a la persona responsable de esta edición
+              </li>
+              <li className="before:content-['🍵']">
+                La persona responsable de esta edición, hace la magia de la redistribución de bolsitas, y envía a cada participante su sobre
+                (previamente vacío) con tantas bolsas como participantes, con mucho té, infusión y amor
+              </li>
+              <li className="before:content-['🍵']">
+                Podéis pensar en qué música, imágenes o historias os gustaría crear para ambientar vuestro día y contribuir al{" "}
+                <span className="font-semibold">
+                  ❝adivina<i>té</i>❞
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <div className="card-body">
+            <h2 className="card-title justify-center font-[Griffy] font-light">
+              <span className="font-extrabold italic ">Caoslendario:</span>Fechas límite{" "}
+              <span className="badge badge-xs badge-error font-sans">¡IMPORTANTE!</span>
+            </h2>
+            <p className="flex-0">Fechas límite:</p>
+            <ul>
+              <li className="before:content-['🫖']">30/09/2025: Cierre de participantes</li>
+              <li className="before:content-['🫖']">
+                31/10/2025: Fecha tope para haber recibido el sobre con las bolsitas individuales y sobre prefranqueado
+              </li>
+              <li className="before:content-['🫖']">
+                15/11/2025: Fecha tope para enviar los detalles inspiradores sobre vuestro té o infusión de vuestro día
+              </li>
+            </ul>
+          </div>
+        </div> */}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
