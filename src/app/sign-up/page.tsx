c724f8d1f76df955d62704188c254642120cf209
Carlos Canet <carlos@carlosca.net>
2025-11-17 22:33:07 +0100

    feat: add BetterAuth with email/password auth and route protection (#6)

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,6 @@
+function SignUpPage() {
+  return (
+    <div>page</div>
+  )
+}
+export default SignUpPage
---
+++ src/lib/auth.ts
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "sqlite" }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      username: {
+        type: "string",
+        required: true,
+        input: true
+      },
+      isAdmin: {
+        type: "boolean",
+        required: true,
+        input: false
+      }
+    }
+  },
+  // socialProviders: {
+  //   google: {
+  //     prompt: "select_account",
+  //     clientId: process.env.GOOGLE_CLIENT_ID as string,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+  //   }
+  // },
+  rateLimit: {
+    window: 10,
+    max: 100
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 31,
+    updateAge: 60 * 60 * 24,
+  },
+  advanced: {
+    cookiePrefix: "advientea"
+  },
+  // trustedOrigins: ["http://localhost:3000", "https://adviente.netlify.app/"],
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL
+});
