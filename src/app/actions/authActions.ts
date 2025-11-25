1efe63d9de71003de5bbc4f1755567c86bd685d4
Carlos Canet <carlos@carlosca.net>
2025-11-25 13:46:07 +0100

    feat: implement user registration page (#7)

---
+++ src/app/actions/authActions.ts
@@ -0,0 +1,113 @@
+"use server";
+import { uploadImageCloudinary } from "./uploadActions";
+import { auth } from "@/lib/auth";
+import { SignInActionResponse, SignInFormData, SignUpActionResponse, SignUpFormData } from "@/lib/definitions";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const SignupFormSchema = z.object({
+  username: z.string().min(2, { error: "Name must be at least 2 characters long." }).trim(),
+  email: z.string().email({ error: "Introduce un email correcto." }).trim(),
+  password: z
+    .string()
+    .min(8, { error: "La contraseña debe ser de al menos 8 caracteres." })
+    .regex(/[a-zA-Z]/, { error: "La contraseña debe contener al menos una letra." })
+    .regex(/[0-9]/, { error: "La contraseña debe contener al menos un número." })
+    .regex(/[^a-zA-Z0-9]/, {
+      error: "Debe contener al menos un caracter especial.",
+    })
+    .trim(),
+  passwordConfirmation: z.string(),
+  image: z
+    .file()
+    .max(2 * 1024 * 1024, { error: "Tamaño de imagen demasiado grande (max 2 MB)." })
+    .mime(["image/jpeg", "image/png", "image/gif", "image/svg+xml", "image/webp"], {
+      error: "Formato de archivo no permitido."
+    }),
+}).refine(data => data.password === data.passwordConfirmation, {
+  error: "Las contraseñas no coinciden.",
+  path: ["passwordConfirmation"],
+});
+
+export async function signup(prevState: SignUpActionResponse | null, formData: FormData): Promise<SignUpActionResponse> {
+  try {
+    const rawData: SignUpFormData = {
+      username: formData.get("username") as string,
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+      passwordConfirmation: formData.get("passwordConfirmation") as string,
+      image: formData.get("image") as File,
+    }
+    const validatedFields = SignupFormSchema.safeParse(rawData)
+
+    if (!validatedFields.success) {
+      return {
+        success: false,
+        message: "Hay errores en el formulario",
+        errors: z.treeifyError(validatedFields.error),
+        inputs: rawData
+      }
+    }
+    const imgPublicId = await uploadImageCloudinary(validatedFields.data.image);
+  
+    const result = await auth.api.signUpEmail({
+      body: {
+        name: validatedFields.data.username,
+        email: validatedFields.data.email,
+        password: validatedFields.data.password,
+        image: imgPublicId,
+        username: validatedFields.data.username,
+        callbackURL: "/profile"
+      }
+    })
+    console.log("Validation success:", result)    
+  } catch (error) {
+    console.error("Validation error:", error)
+    let errorMsg = "";
+    if (error && typeof(error) === "object" && "message" in error) {
+      errorMsg = (error.message === "User already exists. Use another email.") ? "Ya existe un usuario con ese email" : error.message as string;
+    }
+    return {
+      success: false,
+      message: "An error has ocurred",
+      errors: {
+        errors: [errorMsg ? errorMsg : "An error has ocurred"]
+      }
+    }
+  }
+  
+  redirect("/profile");
+}
+
+
+export async function signin(prevState: SignInActionResponse | null, formData: FormData): Promise<SignInActionResponse> {
+  try {
+    const rawData: SignInFormData = {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string
+    }
+    await auth.api.signInEmail({
+      body: {
+        email: rawData.email,
+        password: rawData.password,
+        callbackURL: "/profile"
+      }
+    })
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      message: "An error has ocurred",
+    }
+  }
+  
+  redirect("/profile");
+}
+
+export async function signout() {
+  await auth.api.signOut({
+    headers: await headers(),
+  });
+  redirect("/");
+}
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,142 +1,140 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 import Link from "next/link";
 import Form from "next/form";
-import { FaUser } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaUser } from "react-icons/fa";
 import { GiBrainLeak } from "react-icons/gi";
 import { IoKeySharp } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import Image from "next/image";
 import { CgProfile } from "react-icons/cg";
-import { authClient } from "@/lib/auth-client";
-import { uploadImageCloudinary } from "@/app/actions/upload";
+import { signup } from "@/app/actions/authActions";
+import { initialSignUpActionResponse } from "@/lib/definitions";
 
-const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/gif", "image/svg+xml", "image/webp"];
 
 export default function SignUpForm() {
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
-  const [image, setImage] = useState<File | null>(null);
+  const [state, action, isPending] = useActionState(signup, initialSignUpActionResponse);
   const [imagePreviewURL, setImagePreviewURL] = useState<string>("");
   const [imageErrorMsg, setImageErrorMsg] = useState<string>("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   useEffect(() => {
-    return () => URL.revokeObjectURL(imagePreviewURL);
+    return () => {
+      if (imagePreviewURL) {
+        URL.revokeObjectURL(imagePreviewURL);
+      }
+    };
   }, [imagePreviewURL]);
+
+  useEffect(() => {
+    if (state.errors) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setImagePreviewURL("");
+      setPassword("");
+      setPasswordConfirmation("");
+      setShowPassword(false);
+    }
+  }, [state.errors]);
+  console.log(state.success);
+  const passwordError = 
+    password && passwordConfirmation && password !== passwordConfirmation
+      ? "Las contraseñas no coinciden."
+      : "";
 
   const onAddFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     setImageErrorMsg("");
-    if (event.target.files) {
+    if (event.target.files && event.target.files.length > 0) {
       const imageFile = event.target.files[0];
       if (!ALLOWED_IMAGE_TYPES.includes(imageFile.type)) {
         event.currentTarget.value = "";
         setImageErrorMsg("Tipo de fichero no permitido");
         return;
       }
-      setImage(imageFile);
       setImagePreviewURL(URL.createObjectURL(imageFile));
     }
   };
 
-  const onSubmit = async () => {
-    let imageURL;
-    if (image) {
-      imageURL = await uploadImageCloudinary(image);
-    }
-    //! Handle upload error
-    await authClient.signUp.email({
-      email,
-      password,
-      name: username,
-      username,
-      image: imageURL,
-      callbackURL: "/profile",
-      fetchOptions: {
-        onError: (ctx: unknown) => {
-          console.log("Error", ctx)
-        },
-        onSuccess: () => {
-          console.log("Registrado")
-        }
-      }
-    } as unknown as Parameters<typeof authClient.signUp.email>[0])
-  }
   return (
     <div className="card w-96 bg-base-100 shadow-xl mt-20 mb-20">
-      <Form className="card-body" action={onSubmit}>
+      <Form className="card-body" action={action}>
         <h2 className="card-title">Registro</h2>
         <div className="items-center mt-2">
           <label className="input input-bordered flex items-center gap-2 mb-2 w-full">
             <FaUser />
-            <input type="text" className="grow" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" name="username" className="grow" placeholder="Username" required defaultValue={state?.inputs?.username}/>
           </label>
+          {state?.errors?.properties?.username && <p className="text-xs text-error font-bold -mt-2 mb-2">{state.errors.properties.username.errors.join(" ")}</p>}
+
           <label className="input input-bordered flex items-center gap-2 mb-2 w-full">
             <MdEmail />
-            <input type="text" className="grow" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" name="email" className="grow" placeholder="Email" required defaultValue={state?.inputs?.email} />
           </label>
+          {state?.errors?.properties?.email && <p className="text-xs text-error font-bold -mt-2 mb-2">{state.errors.properties.email.errors.join(" ")}</p>}
+
           <label className="input input-bordered flex items-center gap-2 mb-2 w-full">
             <IoKeySharp />
             <input
-              type="password"
+              name="password"
+              type={showPassword ? "text" : "password"}
               className="grow"
               placeholder="Introduce tu contraseña"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              defaultValue={state?.inputs?.password}
+              required
             />
+            <button type="button" onClick={() => setShowPassword(!showPassword)} className="cursor-pointer">
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </label>
+          {state?.errors?.properties?.password && <p className="text-xs text-error font-bold -mt-2 mb-2">{state.errors.properties.password.errors.join(" ")}</p>}
+
           <label className="input input-bordered flex items-center gap-2 mb-2 w-full">
             <IoKeySharp />
             <input
-              type="password"
+              name="passwordConfirmation"
+              type={showPassword ? "text" : "password"}
               className="grow"
               placeholder="Confirma tu contraseña"
               value={passwordConfirmation}
               onChange={(e) => setPasswordConfirmation(e.target.value)}
+              defaultValue={state?.inputs?.passwordConfirmation}
+              required
             />
           </label>
-          <div className="flex items-center gap-4">
+          {passwordError && <p className="text-xs text-error font-bold -mt-2 mb-2">{passwordError}</p>}
+          {!passwordError && state.errors?.properties?.passwordConfirmation && <p className="text-xs text-error font-bold -mt-2 mb-2">{state.errors.properties.passwordConfirmation.errors.join(" ")}</p>}
+
+          <div className="flex items-center gap-4 mt-4">
             <div className="w-24 h-24 rounded-full overflow-hidden flex items-center justify-center">
-              {!image ? (
+              {!imagePreviewURL ? (
                 <CgProfile size={96} className="text-neutral text-opacity-70" />
               ) : (
                 <Image src={imagePreviewURL} alt="User profile preview" className="object-cover w-full h-full" width={96} height={96} />
               )}
             </div>
             <div className="flex-1 max-w-md">
-              <div className="mb-2">
-                <div className="text-sm opacity-80 mb-1">Seleccionar imagen</div>
-                <div className="flex items-center gap-3">
-                  <input
-                    id="signup-profile-file"
-                    type="file"
-                    className="hidden"
-                    onChange={(e) => onAddFile(e)}
-                    aria-label="Seleccionar imagen de perfil"
-                  />
-                  <label htmlFor="signup-profile-file" className="btn btn-sm btn-primary">
-                    Seleccionar
-                  </label>
-                  <div className="text-xs opacity-70 max-w-26 truncate">{image?.name ?? "Ningún archivo"}</div>
-                </div>
-                {imageErrorMsg && <p className="text-xs text-error font-bold mt-1">{imageErrorMsg}</p>}
-              </div>
+              <div className="text-sm opacity-80 mb-1">Seleccionar imagen</div>
+              <input id="signup-profile-file" name="image" type="file" className="file-input file-input-bordered file-input-primary file-input-sm w-full max-w-xs" onChange={onAddFile} aria-label="Seleccionar imagen de perfil" />
+              {state?.errors?.properties?.image && <p className="text-xs text-error font-bold mt-1">{state.errors.properties.image.errors.join(" ")}</p>}
+              {imageErrorMsg && <p className="text-xs text-error font-bold mt-1">{imageErrorMsg}</p>}
             </div>
           </div>
         </div>
-        <div className="card-actions justify-center">
-          <button className="btn btn-primary w-full" type="submit">
-            Registrarme
+        <div className="card-actions justify-center mt-4">
+          <button className="btn btn-primary w-full" type="submit" disabled={isPending || !!passwordError}>
+            {isPending ? <span className="loading loading-spinner"></span> : "Registrarme"}
           </button>
         </div>
+        {state?.success ? <p className="text-sm text-success">{state.message}</p> : <p className="text-sm text-error">{state.errors?.errors.join(" ")}</p>}
+        <div className="divider"></div>
         <div className="flex gap-2 items-center justify-center">
           <GiBrainLeak />
-          <Link href="/reset-password" className="link-neutral">
-            {" "}
-            He olvidado mi contraseña
-          </Link>
+          <Link href="/reset-password" className="link-neutral"> He olvidado mi contraseña</Link>
         </div>
       </Form>
     </div>
