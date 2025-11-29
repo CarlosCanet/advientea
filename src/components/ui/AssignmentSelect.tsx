a4abc9ca59b67993ac904341d0a5ee42c5e32a02
Carlos Canet <carlos@carlosca.net>
2025-11-29 02:15:13 +0100

    fix: add toast notification for user assignment change in AssignmentSelect component

--- src/components/ui/AssignmentSelect.tsx
+++ src/components/ui/AssignmentSelect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { assignUserToDay } from "@/app/actions/teaInfoActions";
 import { User } from "@/generated/prisma/client";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 interface AssignmentSelectProps {
   userId: string;
@@ -10,22 +10,39 @@ interface AssignmentSelectProps {
   guestName?: string;
 }
 
-function AssignmentSelect({ userId, dayId, users, guestName }: AssignmentSelectProps) {
+export default function AssignmentSelect({ userId, dayId, users, guestName }: AssignmentSelectProps) {
   const [isPending, startTransition] = useTransition();
   const [currentValue, setCurrentValue] = useState<string>(guestName ?? (userId ?? ""));
+  const [showToast, setShowToast] = useState<boolean>(false);
 
   const handleChangeSelect = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     setCurrentValue(event.target.value);
     startTransition(async () => {
       await assignUserToDay(dayId, event.target.value, 2025, guestName);
+      setShowToast(true);
     });
   }
+
+  useEffect(() => {
+    if (showToast) {
+      const timer = setTimeout(() => setShowToast(false), 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [showToast]);
   return (
-    <select value={currentValue} className={`select ${!currentValue ? "text-neutral" : ""}`} name={`person-day-${dayId}`} onChange={(event) => handleChangeSelect(event)} disabled={isPending} >
-      <option value="">Elige a une persone</option>
-      {users.map((user) => <option key={user.id} value={user.id} >{user.username}</option>)}
-      {guestName && <option key={`${dayId}-${guestName}`} value={guestName} >{guestName}</option>}
-    </select>
+    <>
+      <select value={currentValue} className={`select ${!currentValue ? "text-neutral" : ""}`} name={`person-day-${dayId}`} onChange={(event) => handleChangeSelect(event)} disabled={isPending} >
+        <option value="">Elige a une persone</option>
+        {users.map((user) => <option key={user.id} value={user.id} >{user.username}</option>)}
+        {guestName && <option key={`${dayId}-${guestName}`} value={guestName} >{guestName}</option>}
+        </select>
+      {showToast &&
+        <div className="toast">
+          <div className="alert alert-warning">
+            <span>Usuario cambiado</span>
+          </div>
+        </div>
+      }
+    </>
   )
 }
-export default AssignmentSelect
